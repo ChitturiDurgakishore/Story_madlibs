81f2bf859709a2fbb15fa2ed4043384f66066920
ChitturiDurgakishore <chitturidurgakishore@gmail.com>
2024-08-20 23:02:14 +0530

    Add files via upload

---
+++ StoryProject.py
@@ -0,0 +1,10 @@
+village=input("Enter village name:")
+father=input("enter father name:")
+son=input("enter son name:")
+wife=input("enter wife name:")
+restaurent=input("Enter restaurent name:")
+business=input("enter business name:")
+story= (f"This story takes place in a village named {village}.\n In {village} , there is father named {father} . He's spiritually good and has best qualities a father should have except drinking alcohol . Because of alcohol his life has been spoiled .He became financially unstable.\n\n {father} has a small house in which he's living with both his son {son} and his wife {wife}.\n\n His son joined the college in Hyderabad . Due to financial problems {son} couldnt clear all his fee due in the first semister so the college has been calling {son}'s father for fee payment . But {father} can't afford to pay fee in time .\n \n Watching everything happened around {son} joined as a server in a {restaurent} to pay his own fee . {son} worked hard and earned enough money to establish a small  business named {business} \n His small business worked really well and went to become a big company . {son} completed his Btech and worked as a Software engineer but he kept his {business} as primary income and running it successfully \n\n He overcome the financial problems and made his family financially free .\n\n {father} is so proud of his son {son}")
+print(story)
+
+
